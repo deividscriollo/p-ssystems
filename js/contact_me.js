da322a680e5b2c6a7e32c21fe62b5fbc9408a73a
deividscriollo <deividscriollo@gmail.com>
2014-11-06 14:35:35 -0500

    liberacion beta
    modo prueba

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>tu mensaje a sido enviado. </strong>");
+                        .append("<strong>Tu mensaje a sido enviado correctamente. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,7 +45,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", estamos en mantenimiento intenta mas tarde. te pedimos disculpas att: admin P&S Systems!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
